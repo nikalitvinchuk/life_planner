a88d5c110eb2a428416e2f41a57c9f0d50c3b9cd
mkk90367 <119660944+mkk90367@users.noreply.github.com>
2022-12-05 11:24:27 +0100

    Add files via upload
    activity1 po określonym czasie uruchamia mainactivity
    
    Signed-off-by: mkk90367 <119660944+mkk90367@users.noreply.github.com>

---
+++ life_planner/life_planner.Android/Activity1.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using AndroidX.AppCompat.App;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await SimulateStartup();
+        }
+        async Task SimulateStartup()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(8));
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
