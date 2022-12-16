0f707285fa5455d526e075deca48d4e872066d63
nikalitvinchuk <116036914+nikalitvinchuk@users.noreply.github.com>
2022-12-16 23:20:18 +0100

    wizualizacja-funkcjonalność

--- life_planner/life_planner/dodanie.cs
+++ life_planner/life_planner/dodanie.cs
@@ -11,5 +11,7 @@ namespace life_planner
         public string ID { get; set; }
         public string Data { get; set; }
         public string Tresc { get; set; }
+        public string Wybor{ get; set; }
+        public string Typ{ get; set; }
     }
 }
